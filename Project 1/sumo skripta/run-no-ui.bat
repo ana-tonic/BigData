9a98aa0c4423a6d73551217b2d7b206252172403
ana-tonic <tonicana@elfak.rs>
2023-04-05 15:18:32 +0200

    sumo

---
+++ Project 1/sumo skripta/run-no-ui.bat
@@ -0,0 +1,2 @@
+sumo-gui -c osm.sumocfg --fcd-output latitude.xml --fcd-output.signals true --fcd-output.distance true --fcd-output.acceleration true --fcd-output.attributes all --fcd-output.geo --emission-output emission.xml --step-length 1 --end 43200 --threads 4
+
